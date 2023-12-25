744b3a0761a73087f76256f04d9860dc44a789c7
KKKJJJSSS <kj971023@naver.com>
2023-12-25 21:44:37 +0900

    20231225 - 서버, DB 이전

--- BeanboxP-master/src/main/java/Beanbox/Beanbox/controller/TextExtractorController.java
+++ BeanboxP-master/src/main/java/Beanbox/Beanbox/controller/TextExtractorController.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class TextExtractor {
+public class TextExtractorController {
     @Autowired
     public BeanMapper beanMapper;
 
